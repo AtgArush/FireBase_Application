0f12093cfc35d95614d776925f3d9d7fd76824dc
unknown <atg041299@gmail.com>
2020-09-14 21:38:33 +0530

    added files

---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+//TODO: DONE add firebase configuration and export it
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
+export const firebaseConfig = {
+  apiKey: "AIzaSyAlrHDaMItwyue_kHO5rox6_TEKSpghaAQ",
+  authDomain: "mygitapp-4323a.firebaseapp.com",
+  databaseURL: "https://mygitapp-4323a.firebaseio.com",
+  projectId: "mygitapp-4323a",
+  storageBucket: "mygitapp-4323a.appspot.com",
+  messagingSenderId: "823088020591",
+  appId: "1:823088020591:web:bab242a99a6e7049aae19a",
+  measurementId: "G-B0H4SEZ6W6"
+};
